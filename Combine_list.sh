f799127e1bdc5177149f36719628cae824ec6c6c
Xie-Bo-2018 <54306618+Xie-Bo-2018@users.noreply.github.com>
2022-02-25 09:22:01 +0800

    Add files via upload

---
+++ Combine_list.sh
@@ -0,0 +1,16 @@
+pwd='path/to/HX1_refNH1/'
+cd $pwd
+mkdir list; cd list
+
+#######prepare gvcf 
+##Tujia HAN
+ref_fai_path="/path/to/GWHAAAS00000000.genome.fasta.fai"
+ref_chr_list=`cat $ref_fai_path | awk '{print $1}'`
+for chr in $ref_chr_list
+do
+for i in NH1
+do 
+echo "path/to/${i}.${chr}.g.vcf.gz" >> ${chr}.list
+done
+done
+
